520c417031445dd009684ad9341d1f9299977ddd
Tanguri <jungwontwo2@naver.com>
2023-10-30 10:43:02 +0900

    회원 테이블 만들기

--- src/main/java/jpabook/jpashop/Member.java
+++ src/main/java/jpabook/jpashop/Member.java
@@ -3,9 +3,9 @@ package jpabook.jpashop;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -14,4 +14,10 @@ public class Member {
     @Id @GeneratedValue
     private Long id;
     private String username;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 }
