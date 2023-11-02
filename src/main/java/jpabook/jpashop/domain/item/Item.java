37bdcfdf0243c8cc22e1f0a63e0c83b2c5e20de1
Tanguri <jungwontwo2@naver.com>
2023-11-02 14:52:41 +0900

    상품 도메인 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -9,10 +9,11 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
 
     @Id @GeneratedValue
@@ -25,4 +26,25 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    //비즈니스 로직//
+
+    /**
+     *
+     *  stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity+=quantity;
+    }
+
+    /**
+     *
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");}
+        this.stockQuantity=restStock;
+    }
 }
